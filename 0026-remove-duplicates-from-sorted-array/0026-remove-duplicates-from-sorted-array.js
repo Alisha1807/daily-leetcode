db6bbdf630b315eabd2aa23073571d0c1bd91c4a
Alisha1807 <125450835+Alisha1807@users.noreply.github.com>
2026-03-21 09:09:21 +0530

    Time: 0 ms (100%), Space: 58.4 MB (41.27%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+var removeDuplicates = function(nums) {
+    let x=0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]>nums[x]){
+            x=x+1;
+            nums[x] = nums[i];
+        }
+    }
+    return x+1;
+};
+let nums = [0,1,2,2,3,0,4,2];
+console.log(removeDuplicates(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
